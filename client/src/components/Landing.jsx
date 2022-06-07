87feba7eb8a0cbcd1e8950f600af07f0266663fe
l33th <l33th@protonmail.ch>
2022-06-07 15:14:37 +0200

    🖌  input styling 🖌

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
           Welcome to
         </h2>
 
-        <h1 className='text-3xl md:text-6xl 1g:text-8xl font-black uppercase mb-8'>
+        <h1 className='text-3xl md:text-6xl lg:text-8xl font-black uppercase mb-8'>
           Business Pro
         </h1>
         {/* Members */}
@@ -28,19 +28,19 @@ const Landing = () => {
           <div className='flex flex-col md:flex-row justify-center mb-4'>
             {/* <label for='member_email'>Email address</label> */}
             <input
-              className='text-2xl'
+               className='text-lg md:text-2x1 placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 focus:bg-opacity-20 duration-150 rounded-full md:rounded-tr-none md:rounded-br-none mb-4 md:mb-0'
               placeholder='e.g. johndoe@gmail.com'
               type='email'
               name='member[email]'
             />
-          </div>
-          <div className='revue-form-actions'>
             <input
               type='submit'
               value='Join today'
               name='member[subscribe]'
+
+              className='bg-primary rounded-full md:rounded-tl-none md:rounded-bl-none text-lg md:text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 cursor-pointer hover:opacity-75 duration-150'
             />
-          </div>
+            </div>
           <div className='opacity-75 italic'>
             By subscribing, you agree with Revue’s{' '}
             <a target='_blank' href='https://www.getrevue.co/terms'  className='hover:opacity-80 duration-150'>
